06992ede9798b59f47c9850adc8787b2182cb777
RoadyBoi <roaidmustafa999@gmail.com>
2023-07-10 22:47:44 +0300

    Restaurant screen working

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, SafeAreaView, ScrollView, Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -6,123 +7,106 @@ import {Cell, Section, TableView} from 'react-native-tableview-simple';
 const Stack = createStackNavigator();
 
 function HomeScreenCell(props) {
-  const { title, tagline, time, img, action } = props;
   return (
     <Cell
+      highlightUnderlayColor="#ccc"
       backgroundColor="transparent"
-      highlightUnderlayColor="#CCC"
       cellContentView={
-        <View style={styles.cellContentView}>
-          <View style={styles.cellImageView}>
-            <Image source={img} style={styles.cellImage} />
-          </View>
-          <View style={styles.cellTextView}>
-            <Text style={styles.cellTitle}>{title}</Text>
-            <Text style={styles.cellTagline}>{tagline}</Text>
-            <View style={styles.cellEta}>
-            <Text style={styles.cellEtaText}>{time}{"\n"}mins</Text>
+        <View style={{width: "100%"}}>
+            <Image source={props.imgUri} style={styles.image} />
+          <View style={styles.restaurantText}>
+            <Text style={styles.Title}>{props.title}</Text>
+            <View style={{flexDirection:'row', justifyContent:"space-between"}}>
+              <Text style={styles.Tagline}>{props.tagline}</Text>
+               <Text style={styles.ratings}>{props.rating}</Text>
+            </View>
+            <Text style={{fontSize:14, color: props.delivery === 'Free Delivery' ? '#D70F64' : 'grey'}}>Delivery: {props.delivery}</Text>
+            <View style={styles.eta}>
+            <Text style={{textAlign:"center"}}>{props.eta}{"\n"}mins</Text>
             </View>
           </View>
         </View>
       }
-      onPress={action}
+      onPress={props.action}
     />
   )
 }
 
 function RestaurantsScreen({ navigation }) {
   const restaurants = [
-    { title: "Joe's Gelato", tagline: "Dessert, Ice cream, £££", time: "10-30", img: require('./assets/gelato-img.jpg') },
-    { title: "Joe's Diner", tagline: "American, burgers, ££", time: "50+", img: require('./assets/gelato-img.jpg') },
-    { title: "Joe's Café", tagline: "Beverages, Coffee, £", time: "5-10", img: require('./assets/gelato-img.jpg') }
+    { title: "Joe's Gelato", tagline: "Dessert, Ice cream, £££", eta: "10-30", imgUri: require('./assets/gelato-img.jpg'), rating:"⭐ 4.1 (400+)", delivery: "Free Delivery" },
+    { title: "Joe's Diner", tagline: "American, burgers, ££", eta: "50+", imgUri: require('./assets/diner-img.jpeg'), rating:"⭐ 4.4 (1000+)", delivery: "4 £ " },
+    { title: "Joe's Café", tagline: "Beverages, Coffee, £", eta: "5-10", imgUri: require('./assets/cafe-img.jpg'), rating:"⭐ 4.7 (100+)", delivery: "Free Delivery" },
   ];
-
   return (
     <SafeAreaView>
       <ScrollView style={{ height: "100%" }}>
         <TableView>
           <Section hideSeparator={true}>
-            {restaurants.map(restaurants => (
+            {restaurants.map(restaurant => (
               <HomeScreenCell
-                key={restaurants.title}
-                {...restaurants}
-                action={() => navigation.navigate("Menu", { title })}
+                {...restaurant}
+                // action={() => navigation.navigate("Menu", { title })}
               />
             ))}
           </Section>
         </TableView>
       </ScrollView>
     </SafeAreaView>
+  ); 
+}
+export default function App() {
+  return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{
+            headerStyle: {backgroundColor: '#D70F64'},
+            headerTitleStyle: {fontSize: 26, color: 'aliceblue', letterSpacing: 0.25, fontWeight: 400},
+            }}>
+            <Stack.Screen name="Restaurants" component={RestaurantsScreen}  options={{headerTitleAlign:'center'}} />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <StatusBar style="light" />
+      </SafeAreaView>
   );
 }
-
-export default function App() {
-  return(
-    <SafeAreaView>
-    <NavigationContainer>
-     <Stack.Navigator screenOptions={{headerStyle:{backgroundColor: '#D70F64'}}}>
-       <Stack.Screen name = "Restaurants" component = {RestaurantsScreen} options={{headerTitleAlign:'center'}}/>
-       {/* <Stack.Screen name = "Menu" component = {MenuScreen} options={{headerTitleAlign:'center'}}/>
-       <Stack.Screen name = "Details" component = {DetailsScreen} options={{headerTitleAlign:'center'}}/> */}
-     </Stack.Navigator>
-    </NavigationContainer>
-    </SafeAreaView>
-  )
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  cellContentView: {
-
-    borderBottomColor: "#ccc",
-    backgroundColor: "none",
+  image: {
     width: "100%",
+    height: 225,
+    borderRadius: 10,
+    paddingLeft:5,
+    paddingRight: 5
   },
-  cellImageView: {
-    width: "100%",
-    height: 290,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  cellImage: {
-    width: "100%",
-    height: "100%",
-  },
-  cellTextView: {
+  restaurantText: {
     padding: 10,
     width: "100%",
   },
-  cellTitle: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  cellTagline: {
-    fontSize: 14,
-    color: "#666",
-  },
-  cellEta: {
+  eta: {
     position: "absolute",
-    right: 10,
-    top: -30,
+    textAlign: "center",
+    borderRadius: 40,
     backgroundColor: "#fff",
-    paddingTop: 10,
-    paddingBottom: 10,
+    zIndex:9,
     paddingLeft: 20,
     paddingRight: 20,
-    textAlign: "center",
-    borderRadius: 100000,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
+    paddingTop: 10,
+    paddingBottom: 10,
+    right: 20,
+    top: -30,
   },
-  cellEtaText: {
+  Title: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  Tagline: {
+    fontSize: 15,
+    color: "grey",
+  },
+  ratings:{
+    paddingRight: 5,
+    top: -2,
     fontSize: 14,
-    textAlign: "center",
+    
   }
 });
