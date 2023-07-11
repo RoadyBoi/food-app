0958c02c668204fca6fac49680f8dbbfb3f059dd
RoadyBoi <roaidmustafa999@gmail.com>
2023-07-11 17:10:12 +0300

    menu added (not working)

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ function HomeScreenCell(props) {
   )
 }
 
-function RestaurantsScreen({ navigation }) {
+function HomeScreen({ navigation }) {
   const restaurants = [
     { title: "Joe's Gelato", tagline: "Dessert, Ice cream, £££", eta: "10-30", imgUri: require('./assets/gelato-img.jpg'), rating:"⭐ 4.1 (400+)", delivery: "Free Delivery" },
     { title: "Joe's Diner", tagline: "American, burgers, ££", eta: "50+", imgUri: require('./assets/diner-img.jpeg'), rating:"⭐ 4.4 (1000+)", delivery: "4 £ " },
@@ -44,9 +44,9 @@ function RestaurantsScreen({ navigation }) {
         <TableView>
           <Section hideSeparator={true}>
             {restaurants.map(restaurant => (
-              <HomeScreenCell
+              <HomeScreenCell key = {restaurant.title}
                 {...restaurant}
-                // action={() => navigation.navigate("Menu", { title })}
+                action={() => navigation.navigate("MenuScreen", { title: restaurant.title })}
               />
             ))}
           </Section>
@@ -55,6 +55,74 @@ function RestaurantsScreen({ navigation }) {
     </SafeAreaView>
   ); 
 }
+
+function MenuScreenCell(props){
+  return (
+    <Cell
+      highlightUnderlayColor="#ccc"
+      backgroundColor="transparent"
+      cellContentView={
+        <View style={{width: "100%"}}>
+            <Image source={props.image} style={styles.image} />
+          <View style={styles.restaurantText}>
+            <Text style={styles.Title}>{props.title}</Text>
+            <View style={{flexDirection:'row', justifyContent:"space-between"}}>
+              <Text style={styles.Tagline}>{props.price}</Text>
+               <Text style={styles.ratings}>{props.calories}</Text>
+            </View>
+          </View>
+        </View>
+      }
+      onPress={props.action}
+    />
+  )
+}
+
+function MenuScreen({props}){
+  const title = props.route.params
+  const menus = {
+    "Joe's Gelato": [
+    {"title": "Gelato Cups", "contents":[
+    {"title":"Original","price":"3.95","image":require('./assets/gelato-img.jpg'),"calories":"400"},
+    {"title":"Chocolate Hazelnut","price":"4.15","image":require('./assets/gelato-img.jpg'),"calories":"425"},
+    {"title":"Salted Caramel","price":"4.00","image":require('./assets/gelato-img.jpg'),"calories":"416"},
+    {"title":"Mango & Passion Fruit","price":"4.5","image":require('./assets/gelato-img.jpg'),"calories":"376"},
+    {"title":"Mixed Berries","price":"4.25","image":require('./assets/gelato-img.jpg'),"calories":"358"},
+    ]},
+    {"title": "Extra Toppings", "contents":[
+    {"title":"Strawberries","price":"1.15","image":require('./assets/gelato-img.jpg'),"calories":"60"},
+    {"title":"Chocolate Chips","price":"1.25","image":require('./assets/gelato-img.jpg'),"calories":"90"},
+    {"title":"Mango","price":"1.5","image":require('./assets/gelato-img.jpg'),"calories":"130"},
+    {"title":"Hazelnut Sauce","price":"1.85","image":require('./assets/gelato-img.jpg'),"calories":"170"}
+    ]},
+    {"title": "Milk Shakes", "contents":[
+    {"title":"Tiramisu","price":"8.00","image":require('./assets/gelato-img.jpg'),"calories":"600"},
+    {"title":"Mango Sorbet","price":"6.5","image":require('./assets/gelato-img.jpg'),"calories":"480"},
+    ]}
+  ]
+  }
+  return(
+    <SafeAreaView>
+      <ScrollView>
+        <TableView>
+          {menus[title].map(menu =>(
+            <Section header={menu.title}>
+              {menu.contents.map(content=>(
+                <MenuScreenCell
+                {...content}
+                />
+              )
+              )}
+            </Section>
+          )
+          )
+          }
+        </TableView>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
 export default function App() {
   return (
       <SafeAreaView style={{ flex: 1 }}>
@@ -63,7 +131,8 @@ export default function App() {
             headerStyle: {backgroundColor: '#D70F64'},
             headerTitleStyle: {fontSize: 26, color: 'aliceblue', letterSpacing: 0.25, fontWeight: 400},
             }}>
-            <Stack.Screen name="Restaurants" component={RestaurantsScreen}  options={{headerTitleAlign:'center'}} />
+            <Stack.Screen name="Restaurants" component={HomeScreen}  options={{headerTitleAlign:'center'}} />
+            <Stack.Screen name="MenuScreen" component={MenuScreen}  options={{headerTitleAlign:'center'}} />
           </Stack.Navigator>
         </NavigationContainer>
         <StatusBar style="light" />
